a5f388f00bc2a43483211b8330ddf9d96d9165f2
lyhcode <lyhcode@gmail.com>
2012-09-20 04:08:33 -0400

    contents ok

--- js/custom.js
+++ js/custom.js
@@ -136,7 +136,7 @@ jQuery(document).ready(function(){
 	// Needed variables
 	var $map 				= $('#map'),
 		$tabContactClass 	= ('tab-contact'),
-		$address 			= 'Level 13, 2 Elizabeth St, Melbourne Victoria 3000 Australia';
+		$address 			= '台灣雲林縣斗六市雲林科技大學';
 	
 	$content.bind('easytabs:after', function(evt,tab,panel) {
 		if ( tab.hasClass($tabContactClass) ) {
